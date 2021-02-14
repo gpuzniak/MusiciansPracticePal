20bf66d3c418a06323a98a664208ece39559fa33
Greg <gpuzniak@gmail.com>
2021-02-14 16:05:24 -0500

    Basic metronome with random chord spawned on every primary beat.

---
+++ Assets/ChordName.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ChordName : MonoBehaviour
+{
+    public TextMeshProUGUI Text;
+
+    private Vector3 goalPosition;
+    private Color goalColor;
+
+    public void Init(string name, Vector3 position, Transform parent) {
+        this.transform.position = position;
+        this.goalPosition = position;
+        this.transform.SetParent(parent);
+        this.GetComponent<RectTransform>().sizeDelta = Vector2.zero;
+        this.Text.color = new Color(1, 1, 1, 0.0f);
+        this.goalColor = new Color(1, 1, 1, 0.2f) ;
+        this.SetChordName(name);
+    }
+
+    public void SetChordName(string name) {
+        this.Text.text = name;
+    }
+
+    public void GoToPos(Vector3 position) {
+        this.goalPosition = position;
+        this.goalColor = Color.white;
+    }
+
+    private void Update() {
+        this.transform.position = Vector3.Lerp(this.transform.position, this.goalPosition, Time.deltaTime * 10f);
+        this.Text.color = Color.Lerp(this.Text.color, goalColor, Time.deltaTime * 10f);
+    }
+}
---
+++ Assets/Metronome.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Metronome : MonoBehaviour
+{
+    public string[] Chords;
+    public int BPM = 80;
+    
+    public int ChordInterval = 4;
+    public AudioSource AudioSourceA;
+    public AudioSource AudioSourceB;
+    public Image TickImage;
+    public RectTransform CurrentMarker;
+    public RectTransform NextMarker;
+    public ChordName ChordNameBase;
+    public RectTransform Parent;
+
+    private ChordName CurrentChordName;
+    private ChordName NextChordName;
+
+    private float timeOfLastBeat = float.MinValue;
+    private int currentBeat = 0;
+
+    private void Update()
+    {
+        float beatInterval = 60.0f / this.BPM;
+        if(Time.time >= this.timeOfLastBeat + beatInterval) {
+            this.timeOfLastBeat = Time.time;
+            this.PlayTick(this.currentBeat == 0);
+        }
+
+        Color c = this.TickImage.color;
+        this.TickImage.color = new Color(c.r, c.g, c.b, c.a * 0.9f);
+        
+    }
+
+    private void PlayTick(bool emphasize = false) {
+        this.TickImage.color = Color.white;
+
+        this.TickImage.transform.localScale = emphasize ?
+            Vector3.one * 2f :
+            Vector3.one;
+
+        if (emphasize) {
+            if(this.CurrentChordName != null) {
+                Destroy(this.CurrentChordName.gameObject);
+            }
+            if (this.NextChordName != null) {
+                this.CurrentChordName = this.NextChordName;
+                this.CurrentChordName.GoToPos(this.CurrentMarker.transform.position);
+            }
+            this.NextChordName = Instantiate(ChordNameBase);
+            this.NextChordName.Init(this.Chords[Random.Range(0, this.Chords.Length)], this.NextMarker.transform.position, this.Parent);
+            this.AudioSourceA.Play();
+        }
+
+        this.AudioSourceB.Play();
+
+        this.currentBeat++;
+        if(this.currentBeat >= 4) {
+            this.currentBeat = 0;
+        }
+    }
+}
